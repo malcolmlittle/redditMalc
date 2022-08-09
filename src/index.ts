3d1ae130fefbf39daddf95a32c250eb66aeb58ab
Malcolm Little <malcolmvlittle@gmail.com>
2022-08-08 22:51:10 -0700

    created main function to connect to database, run migrations, then run sql

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello malcolm");
+import { MikroORM, RequestContext, RequiredEntityData } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from './mikro-orm.config';
+
+const main = async () => {
+    const orm = await MikroORM.init(mikroConfig); /** connect to the database */
+    // run things in the `RequestContext` handler
+
+    await RequestContext.createAsync(orm.em, async () => {
+        await orm.getMigrator().up(); /** run migrations */
+        // inside this handler the `orm.em` will actually use the contextual fork, created via `RequestContext.createAsync()`
+        const post = orm.em.create(Post, {title: 'my first post'} as RequiredEntityData<Post>);
+        await orm.em.persistAndFlush(post); /** run sql */
+        // console.log("----------sql 2----------");
+        // await orm.em.nativeInsert(Post, { title: 'my first post 2'});     
+    });
+};
+
+main().catch(err => {
+    console.error(err)
+});
