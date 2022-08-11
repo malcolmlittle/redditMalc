db946f244293f3374b328a81ef56fa28de9e4c9d
Malcolm Little <malcolmvlittle@gmail.com>
2022-08-10 21:35:44 -0700

    successfully setup express server with an endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { MikroORM, RequestContext, /** RequiredEntityData  */} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroConfig from './mikro-orm.config';
+import express from "express";
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig); /** connect to the database */
@@ -9,12 +9,14 @@ const main = async () => {
 
     await RequestContext.createAsync(orm.em, async () => {
         await orm.getMigrator().up(); /** run migrations */
-        // inside this handler the `orm.em` will actually use the contextual fork, created via `RequestContext.createAsync()`
-        // const post = orm.em.create(Post, {title: 'my first post'} as RequiredEntityData<Post>);
-        // await orm.em.persistAndFlush(post); /** run sql */
         
-        const posts = await orm.em.find(Post, {});
-        console.log(posts);    
+        const app = express();
+        app.get("/", (_, res) => {
+            res.send("hello")
+        });
+        app.listen(4000, () => {
+            console.log('server started on localhost:4000')
+        });
     });
 };
 
