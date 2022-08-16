b793c83353405d314220a55b786d5b4599a26668
Malcolm Little <malcolmvlittle@gmail.com>
2022-08-15 19:52:04 -0700

    implemented CRUD - read by id

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from "../entities/Post";
 import { MyContext } from "src/types";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class PostResolver {
@@ -8,5 +8,13 @@ export class PostResolver {
     @Query(() => [Post])
     posts(@Ctx() {em}: MyContext): Promise<Post[]> {
         return em.find(Post, {});
+    }
+
+    // graphQL query - CRUD - read by id
+    @Query(() => Post, {nullable: true})
+    post(
+        @Arg("id", () => Int) id: number,
+        @Ctx() {em}: MyContext): Promise<Post | null> {
+        return em.findOne(Post, { id });
     }
 }
