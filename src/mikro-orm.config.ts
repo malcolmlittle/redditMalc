fef25faf800c6efd23853331dc3010d42c136ddf
Malcolm Little <malcolmvlittle@gmail.com>
2022-08-18 18:29:06 -0700

    added User entity to MikroORM object

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,13 +2,14 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
     migrations: {
         path: path.join(__dirname, './migrations'), // path to the folder with migrations
         glob: '!(*.d).{js,ts}', // how to match migration files (all .js and .ts files, but not .d.ts)
       },
-    entities: [Post],
+    entities: [Post, User],
     dbName: 'redditmalc',
     type: 'postgresql',
     debug: !__prod__,
