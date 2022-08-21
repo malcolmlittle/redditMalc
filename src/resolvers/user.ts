bf7b5917489d45d27e150fbf5164f432b1ac4e38
Malcolm Little <malcolmvlittle@gmail.com>
2022-08-20 17:35:13 -0700

    created login mutation with error handling based on UserResponse

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,5 @@
 import { MyContext } from "src/types";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
 import { User } from "../entities/User";
 import { RequiredEntityData } from "@mikro-orm/core";
 import argon2 from "argon2";
@@ -10,6 +10,22 @@ class UsernamePasswordInput {
     username: string;
     @Field()
     password: string;
+}
+@ObjectType()
+class FieldError {
+    @Field()
+    field: string;
+    @Field()
+    message: string;
+}
+
+@ObjectType()
+class UserResponse {
+    @Field(() => [FieldError], {nullable: true})
+    errors?: FieldError[];
+
+    @Field(() => User, {nullable: true})
+    user?: User;
 }
 
 @Resolver()
@@ -28,4 +44,37 @@ export class UserResolver {
         await em.persistAndFlush(user);
         return user;
     }
-}
+
+    // graphQL query
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg("options") options: UsernamePasswordInput,
+        @Ctx() { em }: MyContext
+    ): Promise<UserResponse> {
+        const user = await em.findOne(User, {username: options.username})
+        if (!user) {
+            return {
+                errors: [
+                    {
+                        field: "username",
+                        message: "that username doesn't exist"
+                    }
+                ]
+            }
+        }
+        const valid = await argon2.verify(user.password, options.password);
+        if (!valid) {
+            return {
+                errors: [
+                    {
+                        field: "password",
+                        message: "incorrect password"
+                    }
+                ]
+            }
+        }
+        return {
+            user
+        };
+    }
+};
