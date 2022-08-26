a0416af5fc004b8891fb6b5c93e492a433c09d83
Malcolm Little <malcolmvlittle@gmail.com>
2022-08-25 22:23:17 -0700

    created session auth with cookie via redis client

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import connectRedis from "connect-redis";
+import * as redis from "redis";
+import session from "express-session";
+import { MyContext } from "./types";
+
 
 const main = async () => {
     const orm = await MikroORM.init(mikroConfig); /** connect to the database */
@@ -17,16 +22,38 @@ const main = async () => {
         await orm.getMigrator().up(); /** run migrations */
         
         const app = express();
+
+        const RedisStore = connectRedis(session)
+        const redisClient = redis.createClient({
+            legacyMode: true
+        })
+        await redisClient.connect();
+
+        app.use(
+            session({
+                name: "qid",
+                store: new RedisStore({ client: redisClient as any, disableTouch: true}),
+                cookie: {
+                    maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+                    httpOnly: true,
+                    sameSite: "lax", // defense against cross-site request forgery (CSRF) attacks
+                    secure: __prod__
+                },
+                saveUninitialized: false,
+                secret: "happywednesday",
+                resave: false,
+            })
+        )
         
         const apolloServer = new ApolloServer({
             schema: await buildSchema({
                 resolvers: [HelloResolver, PostResolver, UserResolver],
                 validate: false
             }),
-            context: () => ({ em: orm.em })
+            context: ({ req, res }): MyContext => ({ em: orm.em, req, res })
         });
-
-        apolloServer.applyMiddleware({ app })
+        
+        apolloServer.applyMiddleware({ app });
 
         app.listen(4000, () => {
             console.log('server started on localhost:4000')
